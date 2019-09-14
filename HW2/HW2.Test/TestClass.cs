62aa8499b8ffabaf1a0b04931dacc3295800deb2
Kadir Nour <kadir.nour@wsu.edu>
2019-09-14 02:00:57 -0700

    Renamed "Form1" to "Form". Added a Textbox to the form and changed the form title to "Kadir Nour - 011514510"

---
+++ HW2/HW2.Test/TestClass.cs
@@ -0,0 +1,14 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace HW2.Test
+{
+    [TestFixture]
+    public class FormTests
+    {
+        
+    }
+}
