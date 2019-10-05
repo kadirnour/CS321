564b504d93df471e370c8830cc8d7083585de5c3
Kadir <kadir.nour@wsu.edu>
2019-10-04 21:35:21 -0700

    Define a OnCellChanged event handler to subscribe to the CellPropertyChanged event handler in the Spreadsheet class.
    Define CallEndEdit event handler to set the value from UI to text of the backend cell
    
    Instantiate a Spreadsheet object to hold the given number of cells

--- Spreadsheet_Kadir_Nour/Spreadsheet_Kadir_Nour/Form1.cs
+++ Spreadsheet_Kadir_Nour/Spreadsheet_Kadir_Nour/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CptS321;
 
 namespace Spreadsheet_Kadir_Nour
 {
     public partial class Form1 : Form
     {
+
+        public Spreadsheet spreadsheet;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +35,30 @@ namespace Spreadsheet_Kadir_Nour
                 myGrid.Rows[RowCount - 1].HeaderCell.Value = RowToString;
                 RowCount++;
             }
+
+            spreadsheet = new Spreadsheet(50, 26);
+            spreadsheet.CellPropertyChanged += OnCellChanged;
+
+        }
+
+        /// <summary>
+        /// Sets value of UI grid cell to the value of the backend Cell
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnCellChanged(object sender, PropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets the UI grid cell's value to the backend cell's text
+        /// </summary>
+        /// <param name="sender">DataGridView</param>
+        /// <param name="e">CellEndEdit</param>
+        private void CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
