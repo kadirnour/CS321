0977df3e01eaead12a05359b22bcd7f0f8ee0f91
Kadir <kadir.nour@wsu.edu>
2019-10-19 01:01:25 -0700

    Final version of Homework 6.
    I was following TDD but I was having problems with the branch that I was on and couldn't figure it out. I ended up deleting the old branch and creating this one with the completed assignment code.

--- Spreadsheet_Kadir_Nour/ExpressionTree.Test/ExpressionTreeStandaloneTests.cs
+++ Spreadsheet_Kadir_Nour/ExpressionTree.Test/ExpressionTreeStandaloneTests.cs
@@ -6,42 +6,52 @@ using NUnit.Framework;
 using CptS321;
 using System.Reflection;
 using System.Text;
+using System;
 
 namespace ExpressionTreeStandalone.Test
 {
     [TestFixture]
     public class ExpressionTreeStandaloneTests
     {
-     /// <summary>
-     /// Test case that asserts ExpressionTree.tokens is the correct tokenization of Expression
-     /// </summary>
+        /// <summary>
+        /// Test case that asserts ExpressionTree.tokens is the correct tokenization of Expression
+        /// </summary>
         [Test]
         public void PostfixExpressionTest()
         {
             //Empty Expression test case
             ExpressionTree et = new ExpressionTree("");
-            Assert.AreEqual(et.tokens, null);
-            VariableReference.Instance.ClearDictionary();
+            Assert.AreEqual(et.tokens == null ? "" : String.Join("", et.tokens), "");
 
             //Simple variable node test case
             et = new ExpressionTree("A*B*C");
-            Assert.AreEqual(et.tokens, new string[] { "A", "B", "C", "*", "*" });
-            VariableReference.Instance.ClearDictionary();
+            Assert.AreEqual(String.Join("", et.tokens), "AB*C*");
 
             //Simple constant node test case
             et = new ExpressionTree("1 + 2");
-            Assert.AreEqual(et.tokens, new string[] { "1", "2", "+" });
-            VariableReference.Instance.ClearDictionary();
+            Assert.AreEqual(String.Join("", et.tokens), "12+");
 
             //Simple variable and constant node test case with minus
             et = new ExpressionTree("A - 2");
-            Assert.AreEqual(et.tokens, new string[] { "A", "2", "-" });
-            VariableReference.Instance.ClearDictionary();
+            Assert.AreEqual(String.Join("", et.tokens), "A2-");
 
             //Complex variable and constant node test case with random whitespace
             et = new ExpressionTree(" A  /2 / B/ C /3 / 5/D  ");
-            Assert.AreEqual(et.tokens, new string[] { "A", "2", "B", "C", "3", "5", "D", "/", "/", "/", "/", "/", "/" });
-            VariableReference.Instance.ClearDictionary();
+            Assert.AreEqual(String.Join("", et.tokens), "A2/B/C/3/5/D/");
+
+            //Mixed operator tests below
+            et = new ExpressionTree("(2*3)^1/2");
+            Assert.AreEqual(String.Join("", et.tokens), "23*1^2/");
+
+            et = new ExpressionTree("A2 + B3 - F6 * 2");
+            Assert.AreEqual(String.Join("", et.tokens), "A2B3+F62*-");
+
+            et = new ExpressionTree("A2 + B3 - F6 * 2");
+            Assert.AreEqual(String.Join("", et.tokens), "A2B3+F62*-");
+
+            et = new ExpressionTree("2^(4+1) * A6");
+            Assert.AreEqual(String.Join("", et.tokens), "241+^A6*");
+
 
         }
 
@@ -51,18 +61,23 @@ namespace ExpressionTreeStandalone.Test
             ExpressionTree et = new ExpressionTree("2 * x * 3");
             et.SetVariable("x", "2");
             Assert.AreEqual(2, VariableReference.Instance.VariableDictionary["x"]);
-            VariableReference.Instance.ClearDictionary();
 
             et = new ExpressionTree("x * y * 4");
             et.SetVariable("x", "2");
             et.SetVariable("y", "5");
             Assert.AreEqual(2, VariableReference.Instance.VariableDictionary["x"]);
             Assert.AreEqual(5, VariableReference.Instance.VariableDictionary["y"]);
-            VariableReference.Instance.ClearDictionary();
 
-            //Im not sure how to test this but I know SetVariable will throw an exception
-            //et = new ExpressionTree("");
-            //et.SetVariable("x", "2");
+            //Null expression throws an exception with the message 'No Expression'
+            et = new ExpressionTree("");
+            try
+            {
+                et.SetVariable("x", "2");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "No Expression");
+            }
         }
 
         [Test]
@@ -81,11 +96,17 @@ namespace ExpressionTreeStandalone.Test
             et = new ExpressionTree("1/2/1");
             Assert.AreEqual(.5, et.Evaluate());
 
+            et = new ExpressionTree("(2*3)^1/2");
+            Assert.AreEqual(3, et.Evaluate());
 
-            //this evalutes to 1 - (2 - 3) instead of 1 - 2 - 3
+            et = new ExpressionTree("3*(2+4)/2");
+            Assert.AreEqual(9, et.Evaluate());
+
+            et = new ExpressionTree("1.5 *4");
+            Assert.AreEqual(6, et.Evaluate());
+
             et = new ExpressionTree("1-2-3");
-            Assert.AreEqual(2, et.Evaluate());
+            Assert.AreEqual(-4, et.Evaluate());
         }
-
     }
 }
--- Spreadsheet_Kadir_Nour/SpreadsheetEngine/ExpressionTree.cs
+++ Spreadsheet_Kadir_Nour/SpreadsheetEngine/ExpressionTree.cs
@@ -13,18 +13,15 @@ namespace CptS321
     {
         private Node root;
 
-        public string opPattern = @"(\+)|(\-)|(\*)|(\/)|(\^)";
+        //pattern to check if token is supported operator
+        public string opPattern = @"(\+)|(\-)|(\*)|(\/)|(\^)|(\()|(\))";
+        /*pattern for splitting mathematical expression. Credit: Wiktor Stribiżew on 
+        https://stackoverflow.com/questions/34778288/using-regex-to-split-equations-with-variables-c-sharp
+        */
+        public string splitPattern = @"[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?|[-^+*/()]|\w+";
         public string Expression;
         public string[] tokens;
-
-        /// <summary>
-        /// property for pattern. Returns pattern or appends new operator to pattern string
-        /// </summary>
-        public string Pattern
-        {
-            get { return opPattern; }
-            set { opPattern += value; }
-        }
+        private int tokenCount = 0;
 
         /// <summary>
         /// Constructs tree from given expression
@@ -32,9 +29,16 @@ namespace CptS321
         /// <param name="expression"></param>
         public ExpressionTree(string expression)
         {
-            if(expression == "") { Console.WriteLine("Empty Expression"); return; }
+            if (expression == "") { Console.WriteLine("Empty Expression"); return; }
             this.Expression = expression.Replace(" ", ""); //remove whitespace
-            this.tokens = Regex.Split(this.Expression, opPattern);
+            var matches = Regex.Matches(this.Expression, splitPattern);
+
+            tokens = new string[matches.Count];
+
+            foreach (Match m in matches)
+            {
+                tokens[tokenCount++] = m.Value;
+            }
 
             BuildTree();
         }
@@ -63,7 +67,7 @@ namespace CptS321
         /// </summary>
         /// <param name="node">root node</param>
         /// <returns>double value</returns>
-        private double? Evaluate(Node node)
+        private double Evaluate(Node node)
         {
             return node.Eval();
         }
@@ -72,7 +76,7 @@ namespace CptS321
         /// Evaluates ExpressionTree from root
         /// </summary>
         /// <returns>evaluated double or null</returns>
-        public double? Evaluate()
+        public double Evaluate()
         {
             try
             {
@@ -87,35 +91,125 @@ namespace CptS321
                 throw e;
             }
         }
+
+        /// <summary>
+        /// Returns if op1 has a higher precenence than op2
+        /// </summary>
+        /// <param name="op1">first string</param>
+        /// <param name="op2">Second string</param>
+        /// <returns>true if op1 has a higher precedence</returns>
+        private bool IsHigherPrecedence(string op1, string op2)
+        {
+            //supported types are: +, -, *, / and ()
+            int a = -1, b = -1;
+            switch (op1)
+            {
+                case "+":
+                    a = 1;
+                    break;
+                case "-":
+                    a = 1;
+                    break;
+                case "*":
+                    a = 2;
+                    break;
+                case "/":
+                    a = 2;
+                    break;
+                case "^":
+                    a = 3;
+                    break;
+                case "(":
+                    a = 0;
+                    break;
+                case ")":
+                    a = 4;
+                    break;
+                default:
+                    break;
+            }
+
+            switch (op2)
+            {
+                case "+":
+                    b = 1;
+                    break;
+                case "-":
+                    b = 1;
+                    break;
+                case "*":
+                    b = 2;
+                    break;
+                case "/":
+                    b = 2;
+                    break;
+                case "(":
+                    b = 0;
+                    break;
+                case "^":
+                    b = 3;
+                    break;
+                case ")":
+                    b = 4;
+                    break;
+                default:
+                    break;
+            }
+
+            if (op1 == op2 && (op1 == "(" || op1 == ")"))
+                a = 10;
+
+            return a > b ? true : false;
+
+        }
+
         /// <summary>
         /// Reorders ExpressionTree.tokens to Postfix
         /// </summary>
         private void ToPostFix()
         {
             Stack<string> opStack = new Stack<string>();
-            string[] postfixTokens = new string[tokens.Length];
+            List<string> postfixTokens = new List<string>();
 
             int j = 0;
             foreach (string s in tokens)
             {
-                if(!Regex.IsMatch(s, opPattern))
+                if (!Regex.IsMatch(s, opPattern))
                 {
-                    postfixTokens[j] = s;
-                    j++;
+                    postfixTokens.Add(s);
                 }
                 else
                 {
-                    opStack.Push(s);
+                    if (opStack.Count == 0 || s == "(")
+                    {
+                        opStack.Push(s);
+                    }
+
+                    else if (s == ")")
+                    {
+                        while (opStack.Count != 0 && opStack.Peek() != "(") postfixTokens.Add(opStack.Pop());
+                        if (opStack.Count != 0) opStack.Pop();
+                    }
+                    else if (IsHigherPrecedence(s, opStack.Peek()))
+                    {
+                        opStack.Push(s);
+                    }
+                    else if (!IsHigherPrecedence(s, opStack.Peek()))
+                    {
+                        while (opStack.Count != 0 && s != "(") postfixTokens.Add(opStack.Pop());
+
+                        opStack.Push(s);
+                    }
                 }
             }
 
             while (opStack.Any())
             {
-                postfixTokens[j] = opStack.Pop();
-                j++;
+                postfixTokens.Add(opStack.Pop());
+
             }
 
-            tokens = postfixTokens;
+            tokens = postfixTokens.ToArray();
         }
 
         /// <summary>
@@ -124,19 +218,21 @@ namespace CptS321
         private void BuildTree()
         {
             ToPostFix();
+            VariableReference.Instance.ClearDictionary();
             Stack<Node> nodeStack = new Stack<Node>();
             OperatorFactory opFac = new OperatorFactory();
 
-            foreach(string s in tokens)
+
+            foreach (string s in tokens)
             {
-                if(Regex.IsMatch(s, @"[0-9]"))
+                if (Regex.IsMatch(s, @"^[0-9]*\.?[0-9]"))
                 {
                     nodeStack.Push(new ConstantNode(float.Parse(s)));
                 }
-                else if(Regex.IsMatch(s, @"^[a-zA-z]"))
+                else if (Regex.IsMatch(s, @"\w+"))
                 {
                     nodeStack.Push(new VariableNode(s));
-                    VariableReference.Instance.VariableDictionary.Add(s, null);
+                    VariableReference.Instance.VariableDictionary.Add(s, 0);
                 }
                 else
                 {
